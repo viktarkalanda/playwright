875ef35f62469f9f47f559f73af002b3981d2fcf
viktarkalanda <viktarkalanda@gmail.com>
2026-05-14 18:40:28 +0200

    feat: add NavBar, LoginModal, SignUpModal, ContactModal to second-shop fixtures

--- src/second-shop/fixtures/test-fixtures.ts
+++ src/second-shop/fixtures/test-fixtures.ts
@@ -1,34 +1,50 @@
-import { test as base, expect, Page } from '@playwright/test';
+import { test as base, expect } from '@playwright/test';
 import { HomePage } from '../../pages/second-shop/HomePage';
 import { ProductPage } from '../../pages/second-shop/ProductPage';
 import { CartPage } from '../../pages/second-shop/CartPage';
 import { OrderModal } from '../../pages/second-shop/OrderModal';
+import { NavBar } from '../../pages/second-shop/NavBar';
+import { LoginModal } from '../../pages/second-shop/LoginModal';
+import { SignUpModal } from '../../pages/second-shop/SignUpModal';
+import { ContactModal } from '../../pages/second-shop/ContactModal';
 
 type Pages = {
   secondHomePage: HomePage;
   secondProductPage: ProductPage;
   secondCartPage: CartPage;
   secondOrderModal: OrderModal;
+  secondNavBar: NavBar;
+  secondLoginModal: LoginModal;
+  secondSignUpModal: SignUpModal;
+  secondContactModal: ContactModal;
 };
 
 export type SecondShopFixtures = Pages;
 
 export const test = base.extend<SecondShopFixtures>({
   secondHomePage: async ({ page }, use) => {
-    const home = new HomePage(page);
-    await use(home);
+    await use(new HomePage(page));
   },
   secondProductPage: async ({ page }, use) => {
-    const product = new ProductPage(page);
-    await use(product);
+    await use(new ProductPage(page));
   },
   secondCartPage: async ({ page }, use) => {
-    const cart = new CartPage(page);
-    await use(cart);
+    await use(new CartPage(page));
   },
   secondOrderModal: async ({ page }, use) => {
-    const modal = new OrderModal(page);
-    await use(modal);
+    await use(new OrderModal(page));
+  },
+  secondNavBar: async ({ page }, use) => {
+    await use(new NavBar(page));
+  },
+  secondLoginModal: async ({ page }, use) => {
+    await use(new LoginModal(page));
+  },
+  secondSignUpModal: async ({ page }, use) => {
+    await use(new SignUpModal(page));
+  },
+  secondContactModal: async ({ page }, use) => {
+    await use(new ContactModal(page));
   },
 });
 
